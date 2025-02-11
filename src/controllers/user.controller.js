e7c6076a3028f8bbb9b424ff84dcd82d66e9b690
LuizMartinsDev <luizkwdev@gmail.com>
2025-02-11 19:16:04 -0300

    Aplicacação de Middlewares de validação nos métodos HTTP dos usuários

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,77 +3,81 @@ import mongoose from 'mongoose'
 
 
 export const create = async (req, res) => {
-  const {name, username, email, password, avatar, background} = req.body;
 
-  if(!name || !username || !email || !password || !avatar || !background){
-    res.status(400).send({message: "Submit all fields for registration"})
-  }
+  try{
+    const {name, username, email, password, avatar, background} = req.body;
 
-  const createUser = await createUserInDb(req.body);
-
-  if(!createUser){
-    return res.status(400).send({erro: 'Error Creating user'})
-  }
-
-  res.status(201).send({
-    message: "User created successfully", 
-    user: {
-      id: createUser._id,
-      name,
-      username,
-      email,
-      avatar,
-      background
+    if(!name || !username || !email || !password || !avatar || !background){
+      res.status(400).send({message: "Submit all fields for registration"})
     }
-  })
+
+    const createUser = await createUserInDb(req.body);
+
+    if(!createUser){
+      return res.status(400).send({erro: 'Error Creating user'})
+    }
+
+    res.status(201).send({
+      message: "User created successfully", 
+      user: {
+        id: createUser._id,
+        name,
+        username,
+        email,
+        avatar,
+        background
+      }
+    })
+  } catch(err){
+    res.status(500).send({message: err.message})
+  }
+  
 }
 
 export const findAllUsers = async (req, res) => {
-  const users = await findAllUsersInDb();
-  if( users.length == 0){
-    return res.status(400).send({message: "There are no registered users"})
-  }
 
-  res.status(200).send(users)
+  try {
+    const users = await findAllUsersInDb();
+    if( users.length == 0){
+      return res.status(400).send({message: "There are no registered users"})
+    }
+
+    res.status(200).send(users)
+  } catch(err){
+    res.status(500).send({message: err.message})
+  }
+  
 }
 
 export const findByIdUser = async (req, res) => {
-      const idUser = req.params.id;
 
-      if(!mongoose.Types.ObjectId.isValid(idUser)){ 
-        res.status(400).send({message: 'Invalid id'})
-      }
+  try {
+    const user = req.user
 
-      const user = await findByIdUserInDb(idUser)
-
-      if(!user) {
-        return res.status(400).send({message: "User not found"})
-      }
-
-      res.status(200).send(user)
+    res.status(200).send(user)
+  } catch(err){
+    res.status(500).send({message: err.message})
+  }
+      
 }
 
 export const updateUser = async (req, res) => {
-  const {name, username, email, password, avatar, background} = req.body;
+
+  try{
+    const {name, username, email, password, avatar, background} = req.body;
 
   if(!name && !username && !email && !password && !avatar && !background){
     res.status(400).send({message: "Submit at least one field for update"})
   }
 
-  const id = req.params.id;
-
-  if(!mongoose.Types.ObjectId.isValid(id)){ 
-    res.status(400).send({message: 'Invalid id'})
-  }
-
-  const user = await findByIdUserInDb(id);
-
-  if(!user){
-    return res.status(400).send({message: "User not found"})
-  }
+  const id = req.id;
 
   await updateUserInDb(id, name, username, email, password, avatar, background)
 
   res.status(201).send({message: "User succesfully update!!"})
+  }catch(err){
+    res.status(500).send({message: err.message})
+  }
+  
 }
 
--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,11 +1,11 @@
 import express from 'express'
 import {create, findAllUsers, findByIdUser, updateUser} from '../controllers/user.controller.js'
-
+import {validId, validUser} from '../middlewares/global.middlewares.js'
 const router = express.Router();
 router.post('/', create)
 router.get('/', findAllUsers)
-router.get('/:id', findByIdUser)
-router.patch('/:id', updateUser)
+router.get('/:id', validId, validUser, findByIdUser)
+router.patch('/:id', validId, validUser, updateUser)
 
 
 export default router;
